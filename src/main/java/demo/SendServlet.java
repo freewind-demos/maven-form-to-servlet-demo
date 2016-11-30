6f255aed902f444163f6fce7b187a6a6c8034bf0
Peng.Li <nowind_lee@qq.com>
2016-11-30 21:57:06 +0800

    post data to servlet with correct Chinese charactor

---
+++ src/main/java/demo/SendServlet.java
@@ -0,0 +1,18 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SendServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        System.out.println("Name: " + name);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().append("Your name: " + name);
+    }
+}
